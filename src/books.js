7c9d6ec899bca2309a9f0850284f0f8895daca6d
ShivangiVV <vadavadagishivangi@gmail.com>
2023-09-06 22:41:53 +0530

    Updated Books with react fundamentals

---
+++ src/books.js
@@ -0,0 +1,29 @@
+import img1 from './Images/book-1.jpg'
+import img2 from './Images/book-2.jpg'
+import img3 from './Images/book-3.jpg'
+import './index.css'
+
+
+export const books = [
+    {
+        author: "Rebecca Yarros",
+        title: "Iron Flame (The Empyrean, 2)",
+        img: img1,
+        id: 1
+    },
+    {
+        author: "James Clear",
+        title: "Atomic Habits",
+        img: img2,
+        id: 2
+    },
+    {
+        author: "Robert Greene",
+        title: "The 48 Laws of Power",
+        img: img3,
+        id: 3
+    }
+
+]
+
+
